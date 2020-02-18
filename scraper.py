b0ab592de30376c1732c2b4873fc5e261363749e
Olga <olga.kohanova@gmail.com>
2020-02-18 16:08:15 +0300

    +fun to put result into excel file

--- scraper.py
+++ scraper.py
@@ -1,13 +1,56 @@
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from os import listdir, getcwd
+from os.path import isfile, join
+import openpyxl
+from selenium.webdriver.firefox.options import Options
 
-url = "https://twitter.com/NRWINVESTRussia"
-browser = webdriver.Firefox(executable_path=r'C:\Users\NRW Info\geckodriver.exe')
-browser.get(url)
-time.sleep(10) # js loading page
-html = browser.page_source
-soup = BeautifulSoup(html, 'lxml')
-a = soup.findAll("a", {"class": "css-4rbku5 css-18t94o4 css-901oao r-hkyrab r-1loqt21 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0"})
 
-print(a[1]['title']) #title attribute
+
+URL = "https://twitter.com/NRWINVESTRussia"
+TAG = {"class": "css-4rbku5 css-18t94o4 css-901oao r-hkyrab r-1loqt21 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0"}
+FILE = "test.xlsx"
+
+dirpath = getcwd()
+
+
+def getDataFromHtml(url, tag):
+    options = Options()
+    options.headless = True
+    browser = webdriver.Firefox(options=options, executable_path=r'C:\Users\NRW Info\geckodriver.exe')
+    browser.get(url)
+    time.sleep(5) # js loading page waiting
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    a = soup.findAll("a", tag)
+    browser.quit()
+    return a[1]['title'] #title attribute
+
+     
+def getXlsXFilePath():
+    return [f for f in listdir(dirpath) if isfile(join(dirpath, f)) and f.endswith("test.xlsx") ]
+
+# def addCellsWithValues(rowContent, ws, max):
+#     print(rowContent)
+#     for i, entry in enumerate(rowContent,start=1):
+#         ws.cell(row=max+1, column=i+1, value=entry).border = Border(left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))   
+
+def writeToExel(data):
+    wb = openpyxl.load_workbook(filename=FILE)
+    ws = wb.active #todo unhardcode
+    rowContent = data    
+    ws.cell(row=1, column=1, value=rowContent)   
+    wb.save(FILE)
+
+def main():
+    try:
+        result = getDataFromHtml(URL, TAG)
+        print(result)
+    except Exception as err:
+        print('parsing unsuccessfull', err)
+   
+    writeToExel(result)
+
+if __name__ == "__main__":
+    main()
