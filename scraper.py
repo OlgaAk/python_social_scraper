6780a2b5f9d62a28e6aa804829c676059b7d2a1e
Olga <olga.kohanova@gmail.com>
2020-02-19 16:04:01 +0300

    changed file path and added enum for socila media cells in exel

--- scraper.py
+++ scraper.py
@@ -2,23 +2,29 @@ import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from os import listdir, getcwd
-from os.path import isfile, join
+from os.path import isfile, join, abspath
 import openpyxl
 from selenium.webdriver.firefox.options import Options
+from enum import Enum
 
 
 
 URL = "https://twitter.com/NRWINVESTRussia"
 TAG = {"class": "css-4rbku5 css-18t94o4 css-901oao r-hkyrab r-1loqt21 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0"}
-FILE = "test.xlsx"
+FILE = abspath("./../../Ziele_Marketing.xlsx")
+# dirpath = getcwd()
 
-dirpath = getcwd()
+class SocialMediaPositionInExcel(Enum):
+    twitter = (11, 2)
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
 
 
 def getDataFromHtml(url, tag):
     options = Options()
     options.headless = True
-    browser = webdriver.Firefox(options=options, executable_path=r'C:\Users\NRW Info\geckodriver.exe')
+    browser = webdriver.Firefox(options=options, executable_path=r'C:/Users/NRW Info/geckodriver.exe')
     browser.get(url)
     time.sleep(5) # js loading page waiting
     html = browser.page_source
@@ -40,7 +46,7 @@ def writeToExel(data):
     wb = openpyxl.load_workbook(filename=FILE)
     ws = wb.active #todo unhardcode
     rowContent = data    
-    ws.cell(row=1, column=1, value=rowContent)   
+    ws.cell(row=SocialMediaPositionInExcel.twitter.row, column=SocialMediaPositionInExcel.twitter.column, value=rowContent)   
     wb.save(FILE)
 
 def main():
